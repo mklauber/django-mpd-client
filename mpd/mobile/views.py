c82155b543d4b0b15d10c99bb5266311cfda784a
Matthew Lauber <matt+github@mklauber.com>
2012-01-28 22:38:50 -0500

    Wrote view for listing songs.

--- mpd/mobile/views.py
+++ mpd/mobile/views.py
@@ -1,8 +1,14 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from os import path
+
 from utilities.mpd import MPDClient, MPDError
 from utilities.viewtools import template_only
+
+#Configure logging
+import logging
+logger = logging.getLogger( __name__ )
 
 @template_only( 'controls.html' )
 def controls(request):
@@ -21,4 +27,44 @@ def browse( request, *args, **keywords ):
         c['num_artists'] = stats['artists']
         c['num_albums'] = stats['albums']
     return c
-        
+
+@template_only( 'songs.html' )
+def songs( request, artist=None, album=None, *args, **keywords ):
+    c = RequestContext( request )
+    c['breadcrumbs'] = [
+        {'text': 'home', 'target':reverse('controls') },
+        {'text': 'browse', 'target':reverse('browse') },
+    ]
+    if artist:
+        c['breadcrumbs'].append( {
+            'text': 'artist', 
+            'target':reverse( 'artist',  kwargs={ 'artist': artist } ) 
+        } )
+    if album:
+        c['breadcrumbs'].append( {
+            'text': 'album', 
+            'target':reverse( 'album', kwargs={ 'album': album } ) 
+        } )
+    
+    commands = []
+    if artist: commands.extend(['artist', artist])
+    if album:  commands.extend(['album', album]) 
+    if not commands: commands.extend(['any', ''])
+    
+    with MPDClient().connect("localhost", 6600) as mpd:
+        c['songs'] = mpd.find( *commands )
+        logger.debug( "Song: %s", c['songs'][0] )
+    for song in c['songs']:
+        song['filename'] = path.basename( song['file'] )
+    return c
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
