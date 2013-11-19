7d0aa170ee863ac546fb95a934c66837a91103a2
Matthew Lauber <matt@mklauber.com>
2013-11-19 11:30:30 -0500

    Updated for django version 1.6.

--- mpd/mobile/urls.py
+++ mpd/mobile/urls.py
@@ -1,34 +1,34 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 import os
 
 # View imports
 from mobile.views import albums, artists, browse, controls, current_playlist, playlists, songs, switch_playlist
 
-urlPatterns = patterns( '',
+urlPatterns = patterns('',
 
-    url( r'^$', controls, name="controls" ),
-    url( r'^browse/$', browse, name="browse" ),
-    url( r'^search/$', lambda x: x, name="search" ),
+    url(r'^$', controls, name="controls"),
+    url(r'^browse/$', browse, name="browse"),
+    url(r'^search/$', lambda x: x, name="search"),
 
     # Playlists
-    url( r'playlist/$', current_playlist, name='playlist' ),
-    url( r'playlists/$', playlists, name='playlists' ),
-    url( r'switch/(?P<playlist>.+)/$', switch_playlist, name="switch_playlist"),
+    url(r'playlist/$', current_playlist, name='playlist'),
+    url(r'playlists/$', playlists, name='playlists'),
+    url(r'switch/(?P<playlist>.+)/$', switch_playlist, name="switch_playlist"),
 
     # Browse by artist
-    url( r'browse/artists/$', artists, name="artists" ),
-    url( r'browse/artist/(?P<artist>[^/]+?)/$', albums, name="albums_by_artist" ),
-    
-    url( r'browse/artist/(?P<artist>[^/]+?)/songs/$', songs, name="songs_by_artist" ),
-    url( r'browse/artist/(?P<artist>[^/]+?)/(?P<album>[^/]+?)/$', songs, name="songs_of_artist_album" ),
+    url(r'browse/artists/$', artists, name="artists"),
+    url(r'browse/artist/(?P<artist>[^/]+?)/$', albums, name="albums_by_artist"),
+
+    url(r'browse/artist/(?P<artist>[^/]+?)/songs/$', songs, name="songs_by_artist"),
+    url(r'browse/artist/(?P<artist>[^/]+?)/(?P<album>[^/]+?)/$', songs, name="songs_of_artist_album"),
     # Browse by albums
-    url( r'browse/album/$', albums, name="albums" ),
-    
-    url( r'browse/album/(?P<album>[^/]+?)/$', songs, name="album" ),
+    url(r'browse/album/$', albums, name="albums"),
+
+    url(r'browse/album/(?P<album>[^/]+?)/$', songs, name="album"),
     # Browse all songs
-    url( r'browse/songs/$', songs, name="songs"),
+    url(r'browse/songs/$', songs, name="songs"),
 
     # Include the CSS/JS/Media Files
-   ( r'^m/(?P<path>.*)$', 'django.views.static.serve',
-         { 'document_root': os.path.join(os.getcwd(), 'mobile/media') } ),
+   (r'^m/(?P<path>.*)$', 'django.views.static.serve',
+         { 'document_root': os.path.join(os.getcwd(), 'mobile/media') }),
 )
--- mpd/urls.py
+++ mpd/urls.py
@@ -1,40 +1,40 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from mobile import urls
 
 # Ajax Imports
 from ajax import add_songs, clear_songs, next, play, play_song, prev, remove_songs, repeat, random, save_playlist, status, stop, update_library, volume
 
-ajaxPatterns = patterns( '',
-    url( r'status', status, name="ajaxStatus" ),
-    #Playback Controls
-    url( r'play', play, name="ajaxPlay" ),
-    url( r'prev', prev, name="ajaxPrev" ),
-    url( r'next', next, name="ajaxNext" ),
-    url( r'stop', stop, name="ajaxStop" ),
-    url( r'song/(?P<song_id>\d+)/', play_song, name="ajaxSong" ),
+ajaxPatterns = patterns('',
+    url(r'status', status, name="ajaxStatus"),
+    # Playback Controls
+    url(r'play', play, name="ajaxPlay"),
+    url(r'prev', prev, name="ajaxPrev"),
+    url(r'next', next, name="ajaxNext"),
+    url(r'stop', stop, name="ajaxStop"),
+    url(r'song/(?P<song_id>\d+)/', play_song, name="ajaxSong"),
 
 
 
-    url( r'repeat', repeat, name="ajaxRepeat" ),
-    url( r'random', random, name="ajaxRandom" ),
-    url( r'volume/(?P<volume>\d{1,3})/', volume, name="ajaxVolume" ),
+    url(r'repeat', repeat, name="ajaxRepeat"),
+    url(r'random', random, name="ajaxRandom"),
+    url(r'volume/(?P<volume>\d{1,3})/', volume, name="ajaxVolume"),
 
-    #Playlist Controls
-    url( r'add', add_songs, name="playlistAdd" ),
-    url( r'remove', remove_songs, name="playlistRemove" ),
-    url( r'clear', clear_songs, name="playlistClear" ),
-    url( r'save', save_playlist, name="playlistSave" ),
-    
-    #Misc Commands
-    url( r'update', update_library, name="ajaxUpdate" ),
+    # Playlist Controls
+    url(r'add', add_songs, name="playlistAdd"),
+    url(r'remove', remove_songs, name="playlistRemove"),
+    url(r'clear', clear_songs, name="playlistClear"),
+    url(r'save', save_playlist, name="playlistSave"),
+
+    # Misc Commands
+    url(r'update', update_library, name="ajaxUpdate"),
 )
 
 urlpatterns = patterns('',
-    url( r'', include( urls.urlPatterns ) ),
-    url( r'ajax/', include( ajaxPatterns ) ),
+    url(r'', include(urls.urlPatterns)),
+    url(r'ajax/', include(ajaxPatterns)),
 
 
-     ( r'^m/(?P<path>.*)$', 'django.views.static.serve',
-         { 'document_root': settings.MEDIA_ROOT } ),
+     (r'^m/(?P<path>.*)$', 'django.views.static.serve',
+         { 'document_root': settings.MEDIA_ROOT }),
 )
